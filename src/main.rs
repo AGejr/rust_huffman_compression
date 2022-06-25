b95581a83c40d01c32e446e0642c9182fce6ee76
AGejr <albert.gejr@gmail.com>
2022-06-26 00:53:13 +0200

    the program reads the frequency of bytes in the file, which can be specified by passing the filename as an argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+use std::io::Read;
+use std::env;
+
+fn main() -> io::Result<()> {
+    // take filename as argument and open file
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file = fs::File::open(filename)?;
+
+    let mut frequency_table: [u32; 256] = [0;256];
+    
+    // read file byte by byte and count u8 frequency
+    for byte in file.bytes() {
+        let index: usize = byte.unwrap().into();
+        frequency_table[index] += 1;
+    }
+    
+    // print frequency table
+    for i in 0..frequency_table.len() {
+        if frequency_table[i] != 0 {
+            print!("{} {}\n",i,frequency_table[i]);
+        }
+    }
+    
+    Ok(())
 }
