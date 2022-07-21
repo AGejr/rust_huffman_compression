f6daafed45ff75997abfef8b3128d4063e174f9e
alge <albert.gejr@gmail.com>
2022-07-21 14:48:21 +0200

    added interactive mode and binary arg

--- src/argparse.rs
+++ src/argparse.rs
@@ -3,16 +3,25 @@ pub mod argparser {
     pub struct Config {
         pub input_filename: String,
         pub output_filename: String,
+        pub binary: bool,
+        pub interactive: bool,
         pub verbose: bool,
     }
 
     fn print_help_message() {
         let help_msg = format!(
-            "{} {}\n{}\n{}\n{}\n{}",
-            "usage: rust_huffman_compression -i <input filename>",
-            "[-o <output filename>] [-v] [-h]",
-            "optional arguments:",
+            "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
+            "usage: ",
+            "  rust_huffman_compression --interactive",
+            "  rust_huffman_compression -i <input filename> [optional args]",
+            "required",
+            "  --interactive\t\tuse cli input",
+            "  -i, --input\t\tuse file input",
+            "optional args:",
             "  -o, --output\t\toutput filename",
+            "  -b, --binary\t\tread input file as binary file",
+            "\t\t\tthis option should be used when compressing large files",
+            "\t\t\tor when compressing other filetypes than text files",
             "  -v, --verbose\t\tincrease verbosity",
             "  -h, --help\t\tprint help message and exit",
         );
@@ -34,6 +43,8 @@ pub mod argparser {
         let mut config = Config {
             input_filename: String::from(""),
             output_filename: String::from(""),
+            binary: false,
+            interactive: false,
             verbose: false,
         };
 
@@ -42,6 +53,9 @@ pub mod argparser {
             String::from("--input"),
             String::from("-o"),
             String::from("--output"),
+            String::from("-b"),
+            String::from("--binary"),
+            String::from("--interactive"),
             String::from("-v"),
             String::from("--verbose"),
             String::from("-h"),
@@ -55,16 +69,23 @@ pub mod argparser {
                     argument if argument == arguments.get(1).unwrap() => config.input_filename = self::get_arg_value(args, &arg),
                     argument if argument == arguments.get(2).unwrap() => config.output_filename = self::get_arg_value(args, &arg),
                     argument if argument == arguments.get(3).unwrap() => config.output_filename = self::get_arg_value(args, &arg),
-                    argument if argument == arguments.get(4).unwrap() => config.verbose = true,
-                    argument if argument == arguments.get(5).unwrap() => config.verbose = true,
-                    argument if argument == arguments.get(6).unwrap() => self::print_help_message(),
-                    argument if argument == arguments.get(7).unwrap() => self::print_help_message(),
+                    argument if argument == arguments.get(4).unwrap() => config.binary = true,
+                    argument if argument == arguments.get(5).unwrap() => config.binary = true,
+                    argument if argument == arguments.get(6).unwrap() => config.interactive = true,
+                    argument if argument == arguments.get(7).unwrap() => config.verbose = true,
+                    argument if argument == arguments.get(8).unwrap() => config.verbose = true,
+                    argument if argument == arguments.get(9).unwrap() => self::print_help_message(),
+                    argument if argument == arguments.get(10).unwrap() => self::print_help_message(),
                     _ => (),
                 },
                 None => {
                     continue;
                 }
             }
+        }
+
+        if config.interactive {
+            return config
         }
 
         if config.input_filename.len() == 0 {
